ea2f0906a147e59b3486e852c4d2d47a4dae3f21
Anastasiia-Shvets <144601223+Anastasiia-Shvets@users.noreply.github.com>
2024-02-07 15:14:33 +0000

    Deploying to gh-pages from @ Anastasiia-Shvets/Anastasiia-Shvets-goit-js-hw-100@263c561f50e0a34d96828390a7a7d5496597e82f 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/style-edca1572.js";import{i}from"./assets/vendor-77e16229.js";i();
+import"./assets/style-249b8bd7.js";import{i as t}from"./assets/vendor-77e16229.js";const s=document.querySelector("form");s.addEventListener("submit",o=>{o.preventDefault(),parseInt(s.elements.delay.value);const i=s.elements.state.value;function r(e,l){return new Promise((c,u)=>{setTimeout(()=>{l?c(e):u(e)},e)})}r(i==="fulfilled").then(m,n);function m(e){t.success({title:`✅ Fulfilled promise in ${e}ms`,message:"Promise resolved successfully!"})}function n(e){t.error({title:`❌ Rejected promise in ${e}ms`,message:"Promise rejected!"})}});
 //# sourceMappingURL=commonHelpers2.js.map
